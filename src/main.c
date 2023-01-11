601e2892ac5a25608c5b278db62c45f9dd5d138e
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-01-11 12:43:01 +0300

    Add cli

--- src/main.c
+++ src/main.c
@@ -4,24 +4,49 @@
 #include "readimage/image.h"
 #include "readimage/bmp.h"
 
-int main(void) {
+int PrintUsage() {
+    printf("usage: rimg [-v | --version] [-h | --help]\n");
+    printf("            <image_path>                  \n");
+    return 0;
+}
 
-    const char *filePath = "./data/image.bmp";
+int ReadImage(const char *filePath) {
 
     ByteBuffer *buffer = NULL;
-    if (ByteBuffer_ReadFile(filePath, &buffer) != READFILE_OK) {
-        printf("Error on reading file from '%s'.\n", filePath);
-        return 1;
+    {
+        int status = ByteBuffer_ReadFile(filePath, &buffer);
+        if (status != READFILE_OK) {
+            if (status == READFILE_FILENOTEXITS) {
+                printf("Error: file not exits! '%s'", filePath);
+            } else {
+                printf("Error on reading file from '%s'.\n", filePath);
+            }
+            return 1;
+        }
     }
 
     BMPImage *image = NULL;
-    int status = BMPImage_Read(buffer, &image);
-    if (status != READBMP_OK) {
-        printf("Error on reading BMP image from '%s'.\n", filePath);
-        printf("Status code: %d\n", status);
-        return 1;
+    {
+        int status = BMPImage_Read(buffer, &image);
+        if (status != READBMP_OK) {
+            printf("Error on reading BMP image from '%s'.\n", filePath);
+            printf("Status code: %d\n", status);
+            return 1;
+        }
     }
     BMPImage_PrintInfo(image);
     BMPImage_Free(image);
+
     return 0;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc == 1) {
+        return PrintUsage();
+    } else if (argc >= 2) {
+        return ReadImage(argv[1]);
+    }
+
+    return 1;
 }
