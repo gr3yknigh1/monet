ab34567bd9bad613873a3e423dec2f96faa6aac1
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-01-25 03:32:32 +0300

    Remove image rotation

--- cli/main.c
+++ cli/main.c
@@ -4,48 +4,27 @@
 
 #include "libimage/bmp.h"
 #include "libimage/image.h"
+#include "libimage/io.h"
 
 void print_usage(void) {
     printf("./readimage <source-image>\n");
 }
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
+    if (argc < 2) {
         print_usage();
         return 0;
     }
 
     const char *source_path = argv[1];
-    const char *dest_path = argv[2];
 
-    struct image source_image = {0};
-    struct bmp_header header = {0};
-
-    enum read_status r_status = read_bmp(source_path, &source_image, &header);
-    if (r_status != READ_OK) {
-        fprintf(
-            stderr, "Error while reading image '%s', status code: '%d'\n",
-            source_path, r_status
-        );
-        return 1;
+    struct byte_buffer buffer = {0};
+    enum read_status rf_status = read_file(source_path, &buffer);
+    if (rf_status != READ_OK) {
+        fprintf(stderr, "Can't read file. Exit code: '%d'", rf_status);
     }
 
-    struct image rotated_image = rotate_image(&source_image, ROTATION_LEFT);
-    header.width = rotated_image.width;
-    header.height = rotated_image.height;
-
-    enum write_status w_status = write_bmp(dest_path, &rotated_image, &header);
-    if (w_status != WRITE_OK) {
-        fprintf(
-            stderr,
-            "Error while writing image out to path '%s'. Status code: '%d'\n",
-            dest_path, w_status
-        );
-        return 1;
-    }
-
-    free(source_image.pixels);
-    free(rotated_image.pixels);
+    hex_dump(buffer.data, buffer.size);
 
     return 0;
 }
